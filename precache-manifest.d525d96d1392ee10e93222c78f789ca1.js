cbdc54b58dd3fa5076514d368adfb149057a48ca
Ashu103 <abhishu103@gmail.com>
2020-08-08 20:23:14 +0100

    Updates

---
+++ precache-manifest.d525d96d1392ee10e93222c78f789ca1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a131f98e0ee4dbf8c36eb69ea57264b",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "2cc2f6cc2cc7f489b777",
+    "url": "/monsters-rolodex/static/css/main.77ca52a4.chunk.css"
+  },
+  {
+    "revision": "1bb5dd2a6657ff2ee685",
+    "url": "/monsters-rolodex/static/js/2.b6f2604e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b6f2604e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2cc2f6cc2cc7f489b777",
+    "url": "/monsters-rolodex/static/js/main.1a2f502f.chunk.js"
+  },
+  {
+    "revision": "46fcc5578023f1586c35",
+    "url": "/monsters-rolodex/static/js/runtime-main.7affd505.js"
+  }
+]);
